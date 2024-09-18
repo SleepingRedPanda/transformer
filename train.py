01380b6a6ec9a4c1b7c031d9949a7363e7d24e7e
Danyil Kosenko <kosenko.daniel@gmail.com>
2024-09-18 17:00:27 +0200

    Fixtrain.py

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_trg, max_
         prob = model.project(out[:,-1])
         # Select the token with the highest probability
         _, next_word = torch.max(prob, dim = 1)
-        decoder_input = torch.cat([decoder_input, torch.empty(1.1).type_as(source).fill_(next_word.item()).to(device)], dim=1)
+        decoder_input = torch.cat([decoder_input, torch.empty((1, 1)).type_as(source).fill_(next_word.item()).to(device)], dim=1)
         
         if next_word == eos_idx:
             break
